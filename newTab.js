3d4e247bdd826092b8fb657867fd540237760052
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-10-02 21:03:28 -0700

    Move add fortune option to popup

--- newTab.js
+++ newTab.js
@@ -33,30 +33,6 @@ $(document).ready(function() {
 		// $('#cookiepicture').attr("src", image[1]);
 		
 	});
-	
-	$('#addFortune').on('click', function(event) {
-		var newFortune = $('#newFortune').val()
-		console.log("Added following new fortune: ", newFortune)
-		fortunes.push(newFortune);
-
-		// add Notification or Animation so User knows Fortune entered
-		// ie popup notification
-
-		// later add server, and send fortune to local database 
-		// -- or a hosted database on Amazon AWS? 
-
-		// add a filtering function which filters data before adding to cloud database
-
-		// $.ajax({
-		// 	url: "/someurlhere for the server ---> host server and database somewhere",
-		// 	type: "POST",
-		// 	data: { 'age': newFortune },
-		// 	success: function (result) {
-		// 			// notify user that fortune has been added!
-		// 	}
-		// });
-	});
-
 
 });
 
